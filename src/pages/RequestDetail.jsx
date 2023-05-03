60377d1aa9c18641e8f795515aa6bf88f97e92e9
makifinan <m.akifinan@outlook.com.tr>
2023-05-03 12:04:35 +0300

    components

--- src/pages/RequestDetail.jsx
+++ src/pages/RequestDetail.jsx
@@ -1,30 +1,70 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { Button, Card, Dropdown, Icon, Menu } from 'semantic-ui-react'
+import { Button, Card, Dropdown, Icon } from 'semantic-ui-react'
 import RequestService from '../services/RequestService'
+import PriorityService from '../services/PriorityService'
+import StatuService from '../services/StatuService';
+import DeveloperUserService from '../services/DeveloperUserService'
+
 
 export default function RequestDetail() {
     let { id } = useParams({})
-
+    let optionsPriorities = []
+    let optionsStatus = []
     const [request, setRequest] = useState({})
+    const [priorities, setPriorities] = useState([])
+    const [status, setStatus] = useState([])
+    const [developers, setDevelopers] = useState([])
 
     useEffect(() => {
         let requestService = new RequestService()
         requestService.getById(id).then((result) => {
             setRequest(result.data)
+
+
         })
 
-    },[])
-    const optionsPriority = [
-        { key: 1, text: 'Yüksek', value: 1 },
-        { key: 2, text: 'Orta', value: 2 },
-        { key: 3, text: 'Düşük', value: 3 },
-      ]
+        let priorityService = new PriorityService()
+        priorityService.getPriorities().then((result) => {
+            setPriorities(result.data)
+            optionsPriorities = priorities.map((priority) => ({
+                key: priority.id,
+                text: priority.priorityName,
+                value: 3,
+            }));
+        })
+        
+        let statuService = new StatuService()
+        statuService.getListStatu().then((result) => {
+            setStatus(result.data)
+        })
+
+        let developerUserService = DeveloperUserService()
+        developerUserService.getDeveloperUsers().then((result) => {
+            setDevelopers(result.data)
+        })
+
+    }, [])
+    
+   
+    debugger;
+    /* priorities.map((priority) => {
+        optionsPriorities = [
+            { key: priority.id, text: priority.priorityName, value: 3 }
+            
+
+
+        ]
+        debugger;
+    }
+
+    )*/
+
     const options = [
         { key: 1, text: 'Atama Bekliyor', value: 1 },
         { key: 2, text: 'Atama Yapıldı', value: 2 },
         { key: 3, text: 'Çözüldü', value: 3 },
-      ]
+    ]
 
     return (
         <div className='requestdetail'>
@@ -38,16 +78,47 @@ export default function RequestDetail() {
                         <Card.Description>
                             Açıklama : {request.description}
                         </Card.Description>
-                        <Card.Description> Öncelik : 
-                            <Menu compact>
-                                <Dropdown text={request.priorityId} options={optionsPriority} simple item />
-                            </Menu>
+                        <div style={{margin:'1em'}}>
+                        <Card.Description > Öncelik :
+                            <Dropdown text={request.priorityId}
+                                placeholder='Öncelik'
+                                closeOnEscape
+                                selection
+                                options={priorities.map((priority) => ({
+                                    key: priority.id,
+                                    text: priority.priorityName,
+                                    value: 3,
+                                }))}
+                            />
+
                         </Card.Description>
-                        <Card.Description> Durum : 
-                            <Menu compact>
-                                <Dropdown text={request.statuId} options={options} simple item />
-                            </Menu>
+                        <Card.Description style={{margin:'1em'}} > Developer :
+                            <Dropdown text={request.developerUserId}
+                                placeholder='Developer'
+                                closeOnEscape
+                                selection
+                                options={developers.map((developer) => ({
+                                    key: developer.id,
+                                    text: developer.firstName + ' ' + developer.lastName,
+                                    value: 3,
+                                }))}
+                                
+                            >
+                                
+                            </Dropdown>
+
                         </Card.Description>
+                        <Card.Description style={{margin:'1em'}}> Durum :
+
+                            <Dropdown text={request.statuId}
+                                placeholder='Durum'
+                                closeOnEscape
+                                selection
+                                options={options}
+                            />
+
+                        </Card.Description>
+                        </div>
 
                     </Card.Content>
                     <Card.Content extra>
